dbda803ddbfeb2448d4f65cb6763603b175effe3
Tomi Agunbiade <79267850+agunbiade-ar@users.noreply.github.com>
2024-07-26 15:48:05 +0100

    chapter 4: bug fix: made room for null comparisons

--- eloquent js/ch4/deepComparison.js
+++ eloquent js/ch4/deepComparison.js
@@ -1,20 +1,20 @@
 function deepEqual(obj1, obj2) {
-  let obj1Keys = Object.keys(obj1);
+  if ((obj1 == null || obj2 == null) && obj1 != obj2) {
+    return false;
+  } else if (obj1 === obj2) return true;
+  else if (typeof obj1 == 'object' && typeof obj2 == 'object') {
+    let obj1Keys = Object.keys(obj1);
+    let obj2Keys = Object.keys(obj2);
 
-  let obj2Keys = Object.keys(obj2);
+    if (obj1Keys.length == obj2Keys.length) {
+      for (let item1 of obj1Keys) {
+        if (obj2Keys.includes(item1)) {
+          // console.log(obj1[item1], obj2[item1]);
 
-  if (obj1 === obj2) return true;
-  else if (
-    typeof obj1 == 'object' &&
-    typeof obj2 == 'object' &&
-    obj1Keys.length == obj2Keys.length
-  ) {
-    for (let item1 of obj1Keys) {
-      if (obj2Keys.includes(item1)) {
-        console.log(obj1[item1], obj2[item1]);
-        //if the value of obj1[item1] and obj2[item1] are not equal
-        if (!deepEqual(obj1[item1], obj2[item1])) {
-          return false;
+          //if the value of obj1[item1] and obj2[item1] are not equal
+          if (!deepEqual(obj1[item1], obj2[item1])) {
+            return false;
+          }
         }
       }
     }
@@ -25,5 +25,8 @@ function deepEqual(obj1, obj2) {
 
 let a = { a: 4, b: 8 };
 let b = { a: 8, b: 4 };
+let c = Object.assign({}, a);
 
-console.log(deepEqual(a, b));
+// console.log(deepEqual(a, b));
+// console.log(deepEqual(a, c));
+// console.log(deepEqual(null, null));
